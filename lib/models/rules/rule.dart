960b734d28e5f3444c82599c05b8bcf485a75c22
Secanonm <54258853+secanonm@users.noreply.github.com>
2022-06-01 16:58:39 +0530

    Add files via upload

---
+++ lib/models/rules/rule.dart
@@ -0,0 +1,2 @@
+/// files should be located in the src folder, https://bit.ly/3fA23Yz.
+export '../../src/models/rules/rule.dart';
---
+++ lib/widgets/delegate.dart
@@ -0,0 +1,2 @@
+/// files should be located in the src folder, https://bit.ly/3fA23Yz.
+export '../src/widgets/delegate.dart';
